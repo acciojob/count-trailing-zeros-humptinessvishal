1d88d90e66be875605fc8437c8b91c100fc6f21e
Vishal Kumar <136982736+humptinessvishal@users.noreply.github.com>
2023-12-07 19:34:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 function trailingZeros(n) {
   //your JS code here. If required.
+
+	if (isNaN(n) || n < 0) {
+    
+        return "Give Correct Number";
+    }
+
+    let factorial = 1;
+    let countZeros = 0;
+
+    for (let i = 1; i <= n; i++) {
+        factorial = factorial *  i;
+    }
+
+    while (factorial % 10 === 0) {
+        countZeros++;
+        factorial = factorial / 10 
+    }
+
+	return countZeros;
+	
 }
 
 const input = prompt("Enter a number");
